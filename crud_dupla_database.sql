53a7267e10691defd1b44832b334542338c2dfa4
Lucas-Candinho <131051722+Lucas-Candinho@users.noreply.github.com>
2024-09-23 11:04:15 -0300

    Adicionando banco de dados inicial

---
+++ crud_dupla_database.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE crud_beisola_candinho;
+USE crud_beisola_candinho;
+
+CREATE TABLE professores(
+	id_professor INT PRIMARY KEY NOT NULL,
+    nome_professor VARCHAR(45), 
+    ultimo_nome_professor VARCHAR(45),
+    cpf_professor VARCHAR(45),
+    formacao_elementar_professor VARCHAR(255),
+    maior_titulacao_professor VARCHAR(255)
+);
+
+CREATE TABLE aulas(
+	id_aula INT PRIMARY KEY NOT NULL,
+    nome_aula VARCHAR(255),
+    sala_aula VARCHAR(45),
+    departamento_aula VARCHAR(255),
+    tempo_minutos_aula DECIMAL,
+    assunto_aula VARCHAR(255), -- Serve como um campo para anotações do professor para a aula
+    modulo_aula VARCHAR(255) -- Por brevidade será um varchar, porém em um cenário real poderia ser uma tabela
+);
+
+CREATE TABLE diaria(
+	id_diaria INT PRIMARY KEY NOT NULL,
+	fk_professor INT NOT NULL,
+    FOREIGN KEY (fk_professor) REFERENCES professor(id_professor),
+    fk_aula INT NOT NULL,
+    FOREIGN KEY (fk_aula) REFERENCES aulas(id_aula)
+);
